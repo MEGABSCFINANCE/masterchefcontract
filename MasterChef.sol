bd60cca7b071930d8f4636fef47b92daca1128a7
xventurefinance <88228840+xventurefinance@users.noreply.github.com>
2021-08-18 12:07:41 +0530

    Update MasterChef.sol

--- MasterChef.sol
+++ MasterChef.sol
@@ -310,7 +310,7 @@ contract BEP20 is Context, IBEP20, Ownable {
     }
 }
 
-contract BEXToken is BEP20('MORBEX', 'BEX') {
+contract BEXToken is BEP20('Xventure', 'XVT') {
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
